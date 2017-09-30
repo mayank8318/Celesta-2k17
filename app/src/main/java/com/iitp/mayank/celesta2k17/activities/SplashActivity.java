9b69911897040052215a52d71bc8e03aebe5aef1
Arihant Jain <aridj456@gmail.com>
2017-09-30 17:23:54 +0530

    splash revolting issue resolved

--- app/src/main/java/com/iitp/mayank/celesta2k17/activities/SplashActivity.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/activities/SplashActivity.java
@@ -13,6 +13,11 @@ import android.widget.Toast;
 import com.iitp.mayank.celesta2k17.R;
 import com.iitp.mayank.celesta2k17.utils.NetworkUtils;
 
+import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
+
 /**
  * Created by manish on 26/8/17.
  */
@@ -37,7 +42,7 @@ public class SplashActivity extends Activity {
 
         DownloadImagesAysncTask downloadImage = new DownloadImagesAysncTask();
         downloadImage.execute(new ContextWrapper(getApplicationContext()), this);
-        fetchHighlihtsAsynctask fetchHighlihtsAsynctaskwork = new fetchHighlihtsAsynctask();
+        fetchHighlightsAsynctask fetchHighlihtsAsynctaskwork = new fetchHighlightsAsynctask();
         fetchHighlihtsAsynctaskwork.execute( new ContextWrapper(getApplicationContext()),this);
     }
 
@@ -50,7 +55,14 @@ public class SplashActivity extends Activity {
             {
                 Toast.makeText(getBaseContext(), "Download failed. Please try again later", Toast.LENGTH_LONG).show();
             }
-            handler.postDelayed(action, 1000);
+            int time = 1000;
+            try {
+                GifDrawable splashGif = new GifDrawable(getResources(),R.drawable.splash);
+                time = splashGif.getDuration() - (splashGif.getCurrentPosition())%splashGif.getDuration();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            handler.postDelayed(action, time);
         }
 
         @Override
@@ -65,7 +77,7 @@ public class SplashActivity extends Activity {
     }
 
     //to trigger download task for extracting highlights
-    private  class fetchHighlihtsAsynctask extends  AsyncTask< Object , Void , Boolean >
+    private  class fetchHighlightsAsynctask extends  AsyncTask< Object , Void , Boolean >
     {
         @Override
         protected void onPreExecute() {
@@ -80,7 +92,7 @@ public class SplashActivity extends Activity {
         @Override
         protected void onPostExecute(Boolean value ) {
             //if the data is not uploaded
-            if( value ==false )
+            if(!value)
             {
                 Log.e(SplashActivity.class.getName(),"Can't fetch the data highlights ");
             }
