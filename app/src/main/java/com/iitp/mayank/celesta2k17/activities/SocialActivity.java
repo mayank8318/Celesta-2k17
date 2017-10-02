fdae1b8be77d05414a91820450fd7f3976753f54
Meghna Gupta <meghnagupta64@gmail.com>
2017-10-02 16:57:56 +0530

    Social Links fixed

--- app/src/main/java/com/iitp/mayank/celesta2k17/activities/SocialActivity.java
+++ app/src/main/java/com/iitp/mayank/celesta2k17/activities/SocialActivity.java
@@ -1,8 +1,12 @@
 package com.iitp.mayank.celesta2k17.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.iitp.mayank.celesta2k17.R;
 
@@ -15,5 +19,29 @@ public class SocialActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.social_main);
+
+        ImageView twitter = (ImageView) findViewById(R.id.imageViewtwt);
+        ImageView facebook = (ImageView) findViewById(R.id.imageViewfb);
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.facebook.com/Celesta-2k17-1174695189310909/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://twitter.com/celesta_iitp");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
     }
+
+
 }
